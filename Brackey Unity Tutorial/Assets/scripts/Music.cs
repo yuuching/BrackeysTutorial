2f49bc6d7f9265a88a73b4b156a6a07948c70994
Yuuchi Ng <chiuyenngo@yahoo.com>
2018-10-12 02:42:57 -0700

    added some doot doot

---
+++ Brackey Unity Tutorial/Assets/scripts/Music.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+    public AudioSource infiniteloops;
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        infiniteloops = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+
+    {
+        if (infiniteloops.isPlaying) return;
+        infiniteloops.Play();
+    }
+
+    public void StopMusic()
+    {
+        infiniteloops.Stop();
+    }
+
+}
